b7986ddbc5e6a07789e4f64ea2cc4cbef53ad994
liujianzhang.901 <liujianzhang.901@bytedance.com>
2025-08-22 10:42:39 +0800

    add heic hdr pic
    Change-Id: I7a8aa79b6529720d134c239bec2cf2a425b7a0be

---
+++ push.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+adb push ./app/src/main/assets/apple_apple_gainmap.jpg  sdcard/
+adb push ./app/src/main/assets/apple_iso_gainmap.jpg  sdcard/
+
+adb push ./app/src/main/assets/jpg_hdr_galaxy_1.jpg  sdcard/
+adb push ./app/src/main/assets/jpg_hdr_galaxy_2.jpg  sdcard/
+adb push ./app/src/main/assets/jpg_hdr_mi.jpg  sdcard/
+
+adb push ./app/src/main/assets/jpg_hdr_01.jpg  sdcard/
+adb push ./app/src/main/assets/jpg_hdr_02.jpg  sdcard/
+adb push ./app/src/main/assets/jpg_hdr_03.jpg  sdcard/
+adb push ./app/src/main/assets/jpg_hdr_04.jpg  sdcard/
+adb push ./app/src/main/assets/jpg_hdr_05.jpg  sdcard/
+adb push ./app/src/main/assets/jpg_hdr_06.jpg  sdcard/
+adb push ./app/src/main/assets/jpg_hdr_07.jpg  sdcard/
+adb push ./app/src/main/assets/jpg_hdr_08.jpg  sdcard/
+adb push ./app/src/main/assets/jpg_hdr_09.jpg  sdcard/
+adb push ./app/src/main/assets/jpg_hdr_10.jpg  sdcard/
+
+adb push ./app/src/main/assets/hdr_01.heic  sdcard/
+adb push ./app/src/main/assets/hdr_02.heic  sdcard/
+adb push ./app/src/main/assets/hdr_03.heic  sdcard/
+adb push ./app/src/main/assets/hdr_04.heic  sdcard/
+adb push ./app/src/main/assets/hdr_05.heic  sdcard/
+adb push ./app/src/main/assets/hdr_06.heic  sdcard/
+adb push ./app/src/main/assets/hdr_07.heic  sdcard/
+adb push ./app/src/main/assets/hdr_08.heic  sdcard/
+adb push ./app/src/main/assets/hdr_09.heic  sdcard/
+adb push ./app/src/main/assets/hdr_10.heic  sdcard/
